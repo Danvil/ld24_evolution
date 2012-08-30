070406b2a72c25cb8b8069c641ed301a6066a178
David Weikersdorfer <davidw@danvil.de>
2012-08-30 20:34:38 +0200

    Can set number of blobs for lineup
    Signed-off-by: David Weikersdorfer <davidw@danvil.de>

--- Assets/Scripts/BlobLineup.cs
+++ Assets/Scripts/BlobLineup.cs
@@ -6,18 +6,24 @@ public class BlobLineup : MonoBehaviour {
 
 	public GameObject blobPrefab;
 	
+	public int Count = 20;
+
 	List<GameObject> objs = new List<GameObject>();
 	List<float> offset = new List<float>();
 
 	// Use this for initialization
 	void Start () {
-		for(int y=-2; y<=2; y++) {
-			for(int x=-3; x<=3; x++) {
+		float wh = 0.5f * 0.8f * Globals.LevelRect.width;
+		float hh = 0.5f * 0.8f * Globals.LevelRect.height;
+		float d = Mathf.Sqrt(4.0f*wh*hh / (float)Count);
+		float dh = 0.35f * d;
+		for(float y=-hh+0.5f*d; y<=hh; y+=d) {
+			for(float x=-wh+0.5f*d; x<=wh; x+=d) {
 				GameObject blob = (GameObject)Instantiate(blobPrefab);
 				blob.GetComponent<BlobGenotype>().CreateRandom();
 				blob.transform.position = new Vector3(
-					3*x + Random.Range(-1.0f,+1.0f),
-					3*y + Random.Range(-1.0f,+1.0f),
+					x + Random.Range(-dh,+dh),
+					y + Random.Range(-dh,+dh),
 					0);
 				blob.transform.rotation = Quaternion.AngleAxis(
 					Random.Range(0.0f, 360.0f), Vector3.forward);
